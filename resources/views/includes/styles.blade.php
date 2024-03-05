11d2d8e015da7553eb72ae93c1ead8e52fce0fa7
Ozodbek4252 <ozodbekmcfly@gmail.com>
2024-03-05 13:26:23 +0500

    first commit

---
+++ resources/views/includes/styles.blade.php
@@ -0,0 +1,12 @@
+<!-- App favicon -->
+<link rel="shortcut icon" href="{{ asset('assets/images/logo/vortex-logo-with-background.jpg') }}">
+
+<!-- Bootstrap Css -->
+<link href="{{ asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+<!-- Icons Css -->
+<link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+<!-- App Css-->
+<link href="{{ asset('assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="{{ asset('assets/libs/toastr/toastr.css') }}" />
+
+@livewireStyles
